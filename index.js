547dcd92e6564be81a0837a194110fe8ef9c6503
Tango Wu <wtango@hotmail.com>
2019-01-26 01:06:14 +0800

    update my ip to repo automatically

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const https = require('https');
 const fs = require('fs');
-const config =require('./config');
+const config = require('./config');
+const { exec } = require('child_process');
 
 const ECHO_SERVER = 'https://ipecho.net/plain';
-const HereAmIFile = './HereAmI';
+const HereAmIFile = 'HereAmI';
 
 let address = fs.readFileSync(HereAmIFile, 'utf8');
 
@@ -13,15 +14,13 @@ function hereAmI() {
         if (statusCode !== 200) {
             return;
         }
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
+        let echoIp = '';
+        res.on('data', (chunk) => { echoIp += chunk; });
         res.on('end', () => {
-            if (rawData !== address) {
-                fs.writeFileSync(HereAmIFile, rawData);
-                console.log(`address is not the same, origin: ${address}, echoServer: ${rawData}`);
-                address = rawData;
-            } else {
-                console.log(`addresses are the same: ${rawData}`);
+            if (echoIp !== address) {
+                fs.writeFileSync(HereAmIFile, echoIp);
+                exec(`git add ${HereAmIFile} && git commit -m "I move to ${echoIp}" && git push`);
+                address = echoIp;
             }
         });
     });
