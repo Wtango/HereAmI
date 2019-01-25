a40a2dedf75c809f761e5c099da14e33a164692e
Tango Wu <wtango@hotmail.com>
2019-01-26 01:21:38 +0800

    compatible to lower version of nodejs(v4)

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const https = require('https');
 const fs = require('fs');
 const config = require('./config');
-const { exec } = require('child_process');
+const child_process = require('child_process');
 
 const ECHO_SERVER = 'https://ipecho.net/plain';
 const HereAmIFile = 'HereAmI';
 
-let address = fs.readFileSync(HereAmIFile, 'utf8');
+var address = fs.readFileSync(HereAmIFile, 'utf8');
 
 function hereAmI() {
     https.get(ECHO_SERVER, (res) => {
-        const { statusCode } = res;
+        const statusCode = res.statusCode;
         if (statusCode !== 200) {
             return;
         }
-        let echoIp = '';
+        var echoIp = '';
         res.on('data', (chunk) => { echoIp += chunk; });
         res.on('end', () => {
             if (echoIp !== address) {
                 fs.writeFileSync(HereAmIFile, echoIp);
-                exec(`git add ${HereAmIFile} && git commit -m "I move to ${echoIp}" && git push`);
+                child_process.exec(`git add ${HereAmIFile} && git commit -m "I move to ${echoIp}" && git push`);
                 address = echoIp;
             }
         });
