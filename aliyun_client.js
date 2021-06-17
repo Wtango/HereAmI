87277bd5348ea1c560449d412498f4ec2a9e6d2a
Yangtang Wu <wuyangtang@live.com>
2021-06-17 22:52:58 +0800

    Support add A record for aliyun DNS

---
+++ aliyun_client.js
@@ -0,0 +1,30 @@
+const Core = require('@alicloud/pop-core');
+
+export class AliYunClient {
+  constructor(options) {
+    this.options = options;
+    this.client = new Core({
+      accessKeyId: options.accessKeyId,
+      accessKeySecret: options.accessKeySecret,
+      endpoint: 'https://alidns.aliyuncs.com',
+      apiVersion: '2015-01-09'
+    });
+    this.domainParams = {
+      DomainName: options.DomainName,
+      RR: options.RR,
+      Type: 'A',
+      Value: undefined
+    };
+    this.requestOption = {
+      method: 'POST'
+    };
+  }
+
+  req(action, p) {
+    return this.client.request(action, { ...this.params, ...p }, this.requestOption);
+  }
+
+  addDomainRecord(ip) {
+    return this.req('AddDomainRecord', { Value: ip });
+  }
+}
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const https = require('https');
-const fs = require('fs');
-const config = require('./config');
-const child_process = require('child_process');
+import * as  https from 'https';
+import * as  fs from 'fs';
+import * as  childProcess from 'child_process';
+
+import * as aliyun from './aliyun_client.js';
+import * as  config from './config.json';
 
 const HereAmIFile = 'HereAmI';
 const ipv4Regex = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/;
+
+const accessKeyId = process.env.ACCESS_KEY_ID;
+const accessKeySecret = process.env.ACCESS_KEY_SECRET;
+
+const client = new aliyun.AliYunClient({
+    ...config.aliDns,
+    accessKeyId,
+    accessKeySecret
+});
 
 const ECHO_SERVER = {
     'ipecho.net': {
@@ -26,7 +37,7 @@ const ECHO_SERVER = {
 var address = fs.readFileSync(HereAmIFile, 'utf8');
 
 function syncIp(echoIp) {
-    child_process.exec(`git add ${HereAmIFile} && git commit -m "I move to ${echoIp}" && git push`);
+    childProcess.exec(`git add ${HereAmIFile} && git commit -m "I move to ${echoIp}" && git push`);
 }
 
 function hereAmI() {
@@ -44,6 +55,11 @@ function hereAmI() {
                 console.info(`I move to ${echoIp}`);
                 fs.writeFileSync(HereAmIFile, echoIp);
                 syncIp(echoIp);
+                // TODO: change to addOrUpdateDomainRecord
+                client.addDomainRecord(echoIp)
+                    .then((result) => { console.log('Add domain record success:', result) })
+                    .catch((err) => { console.error('Add domain record error:', err) });
+                // FIXME: only update the cached IP when domain record is updated
                 address = echoIp;
             }
         });
